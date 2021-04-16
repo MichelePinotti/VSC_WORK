eb7178e37332a4bc7198b413132036e91f268bb6
michele <miky03.pinotti@gmail.com>
2021-04-16 15:14:49 +0200

    16_04

--- .vscode/C_Files/Master_Class/es.c
+++ .vscode/C_Files/Master_Class/es.c
@@ -21,16 +21,28 @@ typedef struct action{
     struct action* next;
 }Action;
 
+float visualizzaSpeseMensili(Cart* c, int mese){
+    float spese = 0;
+    while(c != NULL){
+        if(c->month == mese){
+            spese = spese + c->price;
+        }
+        c = c->next;
+    }
+    
+    return spese;
+}
+
 int controlForBuy(float price, float saldo){
     if(saldo > price){
-        return 1;
+        return 1;   //se saldo è maggiore dell'importo di acquisto acquisterà il prodotto
     }else{   
-        return 0;
+        return 0;   //se saldo è minore dell'importo di acquisto non acquisterà il prodotto
     }
 }
 
 Cart* acquista(Cart* c, char* name, float price, int day, int month, int year,int type, float* spese, float* saldo, int supermercato){
-    if(supermercato == 0){
+    if(supermercato == 0){  //se funzione supermercato è disattivata sarà possibile effettuare acquisti
         if(controlForBuy(price, *saldo)){
             Cart* newHead = (Cart*)malloc(sizeof(Cart));
             strcpy(newHead->name, name);
@@ -42,13 +54,13 @@ Cart* acquista(Cart* c, char* name, float price, int day, int month, int year,in
             newHead->next = c;
             *spese = *spese + price;
             *saldo = *saldo - price;
-            printf("\n acquisto eseguito su %s...", name);
+            printf("\n acquisto eseguito su %s al prezzo di %f...", name, price);
             return newHead;
         }else{
             printf("\n soldi sul contro insufficenti per effettuare l'acquisto su %s...", name);
             return c;
         }
-    }else
+    }else   //se funzione supermercato è attivata non sarà possibile effettuare acquisti
         printf("\nfunzione supermercato attiva, disattivala per effettuare acquisti");
 }
 
@@ -65,34 +77,36 @@ void visualizzaAcquisti(Cart* c){
 
 void visualizzaAbbonamenti(Cart* c){
     while(c != NULL){
-        if(c->type == 1)
+        if(c->type == 1)    //se il tipo dell'acquisto è abbonamento stamperà l'acquisto
             printf("\nnome: %s  prezzo: %f  data: %d/%d/%d", c->name, c->price, c->day, c->month, c->year);
         c = c->next;
     }
 }
 
 Action* azioni(Action* a, char* name, float price, float* saldo, float* spese, int supermercato){
-    if(supermercato == 0){
+    if(supermercato == 0){  //se funzione supermercato è disattivata sarà possibile effettuare acquisti
         int numberOfAction;
-        printf("\n quante azioni vuoi comprare: ");
+        printf("\n quante azioni vuoi comprare di %s al prezzo di %f? ", name, price);
         scanf("%d", &numberOfAction);
         fflush(stdin);
         float buy = numberOfAction * price;
-        if(controlForBuy(buy, *saldo)){
-            Action* newHead = (Action*)malloc(sizeof(Action));
-            strcpy(newHead->name, name);
-            newHead->number = numberOfAction;
-            newHead->price = price;
-            newHead->next = a;
-            *spese = *spese + price;
-            *saldo = *saldo - price;
-            printf("\n acquisto dell'azione di %s...", name);
-            return newHead;
-        }else{
-            printf("\n soldi sul contro insufficenti per effettuare l'acquisto dell'azione di %s...", name);
-            return a;
+        if(numberOfAction){
+            if(controlForBuy(buy, *saldo)){
+                Action* newHead = (Action*)malloc(sizeof(Action));
+                strcpy(newHead->name, name);
+                newHead->number = numberOfAction;
+                newHead->price = price;
+                newHead->next = a;
+                *spese = *spese + price;
+                *saldo = *saldo - price;
+                printf("\n acquisto dell'azione di %s al costo di %f...", name, price);
+                return newHead;
+            }else{
+                printf("\n soldi sul contro insufficenti per effettuare l'acquisto dell'azione di %s...", name);
+                return a;
+            }
         }
-    }else
+    }else   //se funzione supermercato è attivata non sarà possibile effettuare acquisti
         printf("\nfunzione supermercato attiva, disattivala per effettuare acquisti");
 }
 
@@ -100,7 +114,7 @@ void trend(Action* a, float* spese){
     float bilancio;
     while(a != NULL){
         
-        float molti = (rand()%100)-20;
+        float molti = (rand()%100)-20;  //estrazione casuale di un numero da -20 a 99 e sarà il prezzo corrente dell'azione acquistata
         bilancio = (molti - a->price) * a->number;
         a = a->next;
     }
@@ -109,11 +123,11 @@ void trend(Action* a, float* spese){
 
 void inviaDenaro(float importo, float* saldo){
     if(controlForBuy(importo, *saldo)){
-        char inutile[13];   //numero della carta a cui inviare denaro ma in realta cala solo i soldi
+        char inutile[13];   //numero della carta avrà 12 cifre massimo 
         printf("\ninserisci il numero della carta a cui inviare i soldi (12 numeri): ");
         scanf("%s", &inutile);
         printf("%s", inutile);
-        if(strlen(inutile) == 12){
+        if(strlen(inutile) == 12){  //se il numero della carta non è di 12 cifre non avverrà la transazione
             printf("\ntransazione eseguita");
             *saldo = *saldo - importo;
         }else{
@@ -128,13 +142,49 @@ void invitaAmico(char* email){
     char* token;
     for(int i=0; email[i]!='\0';i++){
         if(email[i] == '@'){
-            strcpy(token, email+i);
+            strcpy(token, email+i); //se l'email inserita non finisce con "@gmail.com" non invierà l'invito
             break;
         }
     }
     //mettere strcpy per confrontare @gmail.com
 }
 
+void donazioni(float importo, float* saldo){
+    if(controlForBuy(importo, *saldo)){
+        int scelta;
+        do{
+            printf("\na chi vuoi inviare la tua donazione?  1.UNICEF  2.TELETHON  3.SAVE THE CHILDREN :");
+            scanf("%d",&scelta);
+            *saldo = *saldo - importo;
+            if(scelta<1 || scelta>3){
+                printf("\ninserimento non valido...");
+            }
+        }while(scelta<1 || scelta>3);
+        if(scelta == 1){
+            printf("\nil team di UNICEF ti ringrazia per la tua donazione e sicuramente dei bambini gioveranno del tuo bel gesto!");
+        }else if(scelta == 2){
+            printf("\nil team di TELETHON ti ringrazia per la tua donazione e sicuramente dei bambini gioveranno del tuo bel gesto!");
+        }else if(scelta == 3){
+            printf("\nil team di SAVE THE CHILDREN ti ringrazia per la tua donazione e sicuramente dei bambini gioveranno del tuo bel gesto!");
+        }
+    }else{
+        printf("\n soldi sul contro insufficenti per effettuare la donazione");
+    }
+}
+
+void cercaAcquisto(Cart* c, char* name){
+    int flag = 0;   //flag = 0 vuol dire che non ha ancora trovato l'ordine
+    while(c != NULL){
+        if(strcmp(c->name, name) == 0){
+            printf("\nordine trovato: acquisto su %s il %d/%d/%d al costo di %f", c->name, c->day, c->month, c->year, c->price);
+            flag = 1;
+        }
+        c = c->next;
+    }
+    if(flag == 0){
+        printf("\nordine inesistente");
+    }
+}
 int main(){
     int scelta; 
     Cart* cart = NULL;
@@ -142,13 +192,17 @@ int main(){
     float saldo = 0;
     float speseMensili = 0;
     float speseAzioni = 0;
-    int flag = 0;
-    int supermercato = 0;       //se 1 è attivo e blocca tutti gli acquisti 
+    int flag = 0;   //flag per azioni
+    int supermercato = 0;       //se 1 è attiva la funzione e blocca tutti gli acquisti 
+    int giorno = 1;
+    int mese = 1;
+    int anno = 2021;
     do{
+        printf("\n---------------------------------------------------");
         printf("\nSALDO: %f", saldo);
-        printf("\nTOTALE SPESE MENSILI: %f", speseMensili);
+        printf("\nTOTALE SPESE MENSILI: %f", visualizzaSpeseMensili(cart, mese));     
         printf("\nBILANCIO AZIONI:");
-        if(flag == 0)   //se non hai ancora comprato azioni stampa 0
+        if(speseAzioni == 0)   //se non hai ancora comprato azioni stampa 0
             printf("0");
         else 
             trend(actionCart, &speseAzioni);
@@ -156,13 +210,13 @@ int main(){
         printf("\n1.ACQUISTA");   
         printf("\n2.CARICA CARTA");     
         printf("\n3.ACQUISTA AZIONI");  
-        printf("\n4.INVIA o RICEVI DENARO");      
+        printf("\n4.INVIA DENARO");      
         printf("\n5.VISUALIZZA ABBONAMENTI");   
-        printf("\n6.INVITA UN TUO AMICO");      
-        printf("\n7.PIANO DI RISPARMIO");  
-        printf("\n8.ATTIVA FUNZIONE SUPERMERCATO");      
-        printf("\n9.DONAZIONI");  
-        printf("\n10.VISUALIZZA ACQUISTI");    
+        printf("\n6.INVITA UN TUO AMICO");  
+        printf("\n7.ATTIVA FUNZIONE SUPERMERCATO");      
+        printf("\n8.DONAZIONI");  
+        printf("\n9.VISUALIZZA ACQUISTI");    
+        printf("\n10.CERCA ACQUISTO");    
         printf("\n11.!HELP!");       
         printf("\n0.USCITA"); 
 
@@ -170,48 +224,87 @@ int main(){
         scanf("%d", &scelta);
         fflush(stdin);
         printf("---------------------------------------------------");
-
-        if(scelta == 1){
-        cart = acquista(cart, "amazon", 7.99, 25, 03, 2021,1, &speseMensili, &saldo, supermercato);  //acquisto andrà a buon fine perche soldi sufficenti(105 - 7.99 = 0)
-        cart = acquista(cart, "e-bay", 101.99, 4, 5, 2021,0, &speseMensili, &saldo, supermercato);  //acquisto non andrà a buon fine perche soldi insufficenti(97.01 - 101.99 = -8.98 < 0)
-        cart = acquista(cart, "subito", 57.99, 9, 10, 2021,0, &speseMensili,&saldo, supermercato);  //acquisto andrà a buon fine perche soldi sufficenti(97.01 - 57.99 = 39.02 >0)
-        cart = acquista(cart, "apple", 2.99, 12, 10, 2021,1, &speseMensili, &saldo, supermercato);
-        cart = acquista(cart, "e-price", 80.99, 22, 10, 2021,0, &speseMensili, &saldo, supermercato);
-        cart = acquista(cart, "apple", 2.99, 12, 11, 2021,1, &speseMensili, &saldo, supermercato);
-        cart = acquista(cart, "amazon", 7.99, 25, 12, 2021,1, &speseMensili, &saldo, supermercato);
-
-        }else if(scelta == 2){
-            ricarica(&saldo, 502);
-        }else if(scelta == 3){
-            flag = 1;
-            actionCart = azioni(actionCart, "tesla", 25.00, &saldo, &speseAzioni, supermercato);
-            actionCart = azioni(actionCart, "ktm", 20.00, &saldo, &speseAzioni, supermercato);
-            actionCart = azioni(actionCart, "ferrari", 35.00, &saldo, &speseAzioni, supermercato);
-        }else if(scelta == 4){
-            inviaDenaro(25.00, &saldo);
-        }else if(scelta == 5){
-            visualizzaAbbonamenti(cart);
-        }else if(scelta == 6){
-            invitaAmico("miky03.pinotti@gmail.com");
-            invitaAmico("francesco.pradella@gmali.com");
-        }else if(scelta == 7){
+        if(scelta<1 || scelta>11){
+            printf("\nfunzionalita' non disponibile... riprova!");
+        }else{
+            if(scelta == 1){
+            char name[10];
+            float price;
+            int abbonamento;
+            printf("\ninserisci il nome del sito su cui vuoi effettuare l'ordine: ");
+            scanf("%s", &name);
+            fflush(stdin);
+            printf("inserisci il prezzo: ");
+            scanf("%f", &price);
+            fflush(stdin);
+            printf("\nstai effettuando un abbonamento? digita 1 se la tua risposta e' 'si' se no digita 0...");
+            scanf("%d", &abbonamento);
+            fflush(stdin);
+            cart = acquista(cart, name, price, giorno, mese, anno, abbonamento, &speseMensili, &saldo, supermercato);
             
-        }else if(scelta == 8){
-            if(supermercato == 0){
-                supermercato = 1;
-                printf("\nfunzione supermercato attivata");
-            }else{
-                supermercato = 0;
-                printf("\nfunzione supermercato disattivata");
+            // cart = acquista(cart, "amazon", 7.99, 25, 1, 2021, 1, &speseMensili, &saldo, supermercato);  //acquisto di tipo abbonamento
+            // cart = acquista(cart, "e-bay", 101.99, 4, 1, 2021, 0, &speseMensili, &saldo, supermercato);  //acquisto di tipo normale
+            // cart = acquista(cart, "subito", 57.99, 9, 1, 2021, 0, &speseMensili, &saldo, supermercato);  //acquisto di tipo normale
+            // cart = acquista(cart, "apple", 2.99, 12, 2, 2021, 1, &speseMensili, &saldo, supermercato);  //acquisto di tipo abbonamento
+            // cart = acquista(cart, "e-price", 80.99, 2, 10, 2021, 0, &speseMensili, &saldo, supermercato);   //acquisto di tipo normale
+            // cart = acquista(cart, "apple", 2.99, 12, 3, 2021, 1, &speseMensili, &saldo, supermercato);      //acquisto di tipo abbonamento
+            // cart = acquista(cart, "amazon", 7.99, 25, 4, 2021, 1, &speseMensili, &saldo, supermercato);     //acquisto di tipo abbonamento
+
+            }else if(scelta == 2){
+                ricarica(&saldo, 300);
+            }else if(scelta == 3){
+                flag = 1;
+                actionCart = azioni(actionCart, "tesla", 25.00, &saldo, &speseAzioni, supermercato);
+                actionCart = azioni(actionCart, "ktm", 20.00, &saldo, &speseAzioni, supermercato);
+                actionCart = azioni(actionCart, "ferrari", 35.00, &saldo, &speseAzioni, supermercato);
+            }else if(scelta == 4){
+                inviaDenaro(25.00, &saldo);
+            }else if(scelta == 5){
+                visualizzaAbbonamenti(cart);
+            }else if(scelta == 6){
+                invitaAmico("miky03.pinotti@gmail.com");
+                invitaAmico("francesco.pradella@gmali.com");
+            }else if(scelta == 7){
+                if(supermercato == 0){
+                    supermercato = 1;
+                    printf("\nfunzione supermercato attivata");
+                }else{
+                    supermercato = 0;
+                    printf("\nfunzione supermercato disattivata");
+                }
+            }else if(scelta == 8){
+                donazioni(20.00, &saldo);
+                donazioni(5.00, &saldo);
+            }else if(scelta == 9){
+                printf("\nACQUISTI");
+                visualizzaAcquisti(cart);
+            }else if(scelta == 10){
+                char name[10];
+                printf("\ninserisci il sito su cui hai effettuato l'acquisto: ");
+                scanf("%s", &name);
+                fflush(stdin);
+                cercaAcquisto(cart, name);
+            }else if(scelta == 11){
+
+                printf("\n1.ACQUISTA = effettua un acquisto di qualsiasi tipo!");   
+                printf("\n2.CARICA CARTA = carica la tua MasterClass dove vuoi quando vuoi!");     
+                printf("\n3.ACQUISTA AZIONI = non farti scappare le opportunita che il mercato ti offre, corri subito ad acquistare azioni e monitorarle con il tuo contro MasterClass!");  
+                printf("\n4.INVIA = invia a un tuo amico un regalo! ");      
+                printf("\n5.VISUALIZZA ABBONAMENTI = visualizza gli abbonamenti attivi per tenere sempre sotto controllo le tue finanze!");   
+                printf("\n6.INVITA UN TUO AMICO = invia tramite email un invito ad unirsi a MasterClass!");      
+                printf("\n8.ATTIVA FUNZIONE SUPERMERCATO = puoi congelare temporaneamente i soldi sul tuo contro MasterClass per non farteli rubare in luoghi pubblici!");      
+                printf("\n9.DONAZIONI = ricorda che ci sono sempre persone che stanno peggio di noi, aiutale tramite una piccola donazione!");  
+                printf("\n10.VISUALIZZA ACQUISTI = visualizza la cronologia di tutti gli acquisti fatti con MasterClass");    
             }
-        }else if(scelta == 9){
-            
-        }else if(scelta == 10){
-            printf("\nACQUISTI");
-            visualizzaAcquisti(cart);
-            printf("\n---------------------------------------------------");
-        }else if(scelta == 10){
-            
+            giorno = giorno +5;     //metto +10 in modo da rendere piu veloce il programma
+            if(giorno >= 31){
+                mese = mese + 1;
+                giorno = 1;
+            }else if(mese >= 12){
+                anno = anno + 1;
+                giorno = 1;
+                mese = 1;
+            }
         }
     }while(scelta != 0);
 
